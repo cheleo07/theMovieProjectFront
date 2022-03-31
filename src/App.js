f16dd25dfab0940141892c13f77236a3d14ea15b
rodica <adigbononrodicaa@gmail.com>
2022-03-31 11:43:33 +0200

    begin

--- src/App.js
+++ src/App.js
@@ -1,10 +1,72 @@
-import logo from './logo.svg';
+import React from "react";
+import './App.css';
+class App extends React.Component {
+
+  // Constructor
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      items: [],
+      DataisLoaded: false
+    };
+  }
+
+  // ComponentDidMount is used to
+  // execute the code
+  componentDidMount() {
+      const requestOptions = {
+          method: 'GET',
+          headers: { 'Content-Type': 'application/json',
+                     'Access-Control-Allow-Origin': 'http://localhost:3000',
+              'Access-Control-Allow-Credentials' : 'true'
+          }
+      };
+    fetch(
+        "http://localhost:3005/api/discover/movie", requestOptions)
+        .then((res) => res.json())
+        .then((json) => {
+            console.log('json ', json)
+          this.setState({
+            items: json,
+            DataisLoaded: true
+          });
+        })
+  }
+  render() {
+    const { DataisLoaded, items } = this.state;
+    console.log("items ", items)
+    /*if (!DataisLoaded) return <div>
+      <h1> Pleses wait some time.... </h1> </div> ;*/
+
+    return (
+        <div className = "App">
+          <h1> Fetch data from an api in react </h1>
+            Page : { items.page}
+            Total pages : { items.total_pages}
+        </div>
+    );
+  }
+}
+
+export default App;
+
+
+/*import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+
+    </div>
+  );
+}
+
+export default App;*/
+
+/*
+* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,8 +80,4 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
-  );
-}
-
-export default App;
+* */
